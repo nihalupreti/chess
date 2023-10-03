62d2db4bbfe66834c62c6fa9f89d92efd0dd93fc
Nihal upreti <n.ytuchiha@gmail.com>
2023-10-03 21:14:31 +0545

    added pawn movement mechanism

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ const chessboard = [
   ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
 ];
 
-// Function to convert indices to chess coordinates (e.g., 0,0 to "a8")
+// convert indices to chess coordinates (e.g., 0,0 to "a8")
 function convertToChessCoordinates(row, col) {
   const chessFile = String.fromCharCode('a'.charCodeAt(0) + col);
   const chessRank = 8 - row;
   return `${chessFile}${chessRank}`;
 }
 
-// Function to initialize the chessboard
 function initializeChessboard() {
   const chessboardElement = document.querySelector('.chessboard');
 
@@ -43,5 +42,32 @@ function initializeChessboard() {
   }
 }
 
-// Call the initialization function
 initializeChessboard();
+
+function pawnMovement(fromX, fromY, toX, toY){
+  const fromPosition = document.getElementById(`square-${fromX}-${fromY}`);
+  const toPosition = document.getElementById(`square-${toX}-${toY}`);
+  const initialPositionBlack = document.getElementById(`square-${6}-${fromY}`);
+  const initialPositionWhite = document.getElementById(`square-${1}-${fromY}`);
+  const initialStepRule =  ((initialPositionBlack.querySelector(".piece") || initialPositionWhite.querySelector(".piece")) && ((fromX == toX+2)||(fromX == toX-2)) && (toY == fromY))
+  const generalMovementRule = (((fromX == toX+1) || (fromX == toX-1)) && (toY == fromY))
+  // var currentPositionLog = {position:fromPosition}
+
+  //check if the pawn move is legal
+  if( generalMovementRule || initialStepRule){
+    //check if the current square has pawn and the toposition is empty
+    if(fromPosition.querySelector(".piece") && !toPosition.querySelector(".piece")){
+      const piece = fromPosition.querySelector(".piece");
+      toPosition.append(piece);
+      fromPosition.innerHTML = "";
+      // currentPositionLog["position"] = toPosition;
+    } 
+  }
+  else{
+    alert("not valid")
+  }
+
+}
+
+// pawnMovement(6,1,4,1)
+
